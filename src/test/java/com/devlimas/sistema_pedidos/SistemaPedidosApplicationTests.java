00e4572bd05e126614d1bbf6794c5e2d2582c92f
Eduardo de Lima R. <lima01dev@gmail.com>
2026-03-15 13:04:06 -0300

    chore: configurações do projeto

---
+++ src/test/java/com/devlimas/sistema_pedidos/SistemaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlimas.sistema_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
