b443e9d09e33348ac89c1dd7652573e5b6fb7335
Eduardo de Lima R. <lima01dev@gmail.com>
2026-03-23 16:07:40 -0300

    refactor: add findByOrderDateBetween to OrderRepository

--- src/main/java/com/devlimas/repositories/OrderRepository.java
+++ src/main/java/com/devlimas/repositories/OrderRepository.java
@@ -4,9 +4,12 @@ import com.devlimas.model.entities.Customer;
 import com.devlimas.model.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByCustomerId(Long customerId);
+
+    List<Order> findByOrderDateBetween(LocalDate startDate, LocalDate endDate);
 }
